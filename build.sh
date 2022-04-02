43478782c3308c404004ba751d9452f4b37b6a69
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 01:32:04 -0400

    creating skel

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 
 CHARTVERSION=`cat VERSION | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/\1/"`
 LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/"`
-echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION
 echo $LOCALREVISION
 
 LOCALREVISION=`echo $LOCALREVISION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-echo $LOCALREVISION
+echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION
+cat VERSION
 
 rm jenkins-${CHARTVERSION}.tgz
 rm -rf charts/jenkins
