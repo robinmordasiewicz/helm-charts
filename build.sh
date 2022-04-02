a9c14b9378c6ca44a246539aa6c6b0725ea33a65
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-01 23:47:20 -0400

    creating skel

--- build.sh
+++ build.sh
@@ -2,10 +2,13 @@
 #
 
 CHARTVERSION='3.11.8'
+cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > VERSION.tmp && mv VERSION.tmp VERSION
+version=`cat VERSION`
 
-rm index.yaml
+#rm index.yaml
 rm jenkins-${CHARTVERSION}.tgz
 rm -rf charts/jenkins
+
 helm repo rm jenkins
 helm repo rm jenkinsci
 helm repo add jenkins https://charts.jenkins.io
@@ -15,15 +18,12 @@ tar -zxvf jenkins-${CHARTVERSION}.tgz -C charts/
 
 cp values.yaml charts/jenkins/
 
-cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > VERSION.tmp && mv VERSION.tmp VERSION
-
-version=`cat VERSION`
-
 cat charts/jenkins/Chart.yaml | sed -re "s/^version: ([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/version: \1-${version}/" > charts/jenkins/Chart.yaml.tmp && mv charts/jenkins/Chart.yaml.tmp charts/jenkins/Chart.yaml
 
 helm package charts/*
 
-helm repo index --url https://github.com/robinmordasiewicz/helm-charts .
+#helm repo index --url https://github.com/robinmordasiewicz/helm-charts .
+helm repo index --url https://robinmordasiewicz.github.io/helm-charts .
 
 git add . && git commit -m "creating skel" &&  git push
 
