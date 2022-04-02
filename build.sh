773ff250ac2599145e31096c97366215cd912ff3
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-01 23:07:08 -0400

    creating skel

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ CHARTVERSION='3.11.8'
 #tar -zxvf jenkins-${CHARTVERSION}.tgz -C charts/
 #rm jenkins-${CHARTVERSION}.tgz
 
+rm index.yaml
 cp values.yaml charts/jenkins/
 
 cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > VERSION.tmp && mv VERSION.tmp VERSION
@@ -21,7 +22,6 @@ cat charts/jenkins/Chart.yaml | sed -re "s/^version: ([0-9]+\.[0-9]+\.[0-9]+)-*[
 
 helm package charts/*
 
-rm index.yaml
 
 helm repo index --url https://github.com/robinmordasiewicz/helm-charts .
 
