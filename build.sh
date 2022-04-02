906fdb8c32f63d4ee12c0f277124fe69164b5dcf
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-01 23:03:04 -0400

    creating skel

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length
 
 version=`cat VERSION`
 
-cat charts/jenkins/Chart.yaml | sed -re "s/^version: ([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/version: \1-${version}/" > charts/jenkins/Chart.yaml.tmp && mv charts/jenkins/Chart.yaml.tmp charts/jenkins/Chart.yaml
+cat charts/jenkins/Chart.yaml | sed -re "s/^version: ([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*$/version: \1-${version}/" > charts/jenkins/Chart.yaml.tmp && mv charts/jenkins/Chart.yaml.tmp charts/jenkins/Chart.yaml
 
 helm package charts/*
 
