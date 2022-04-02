4a019cd8fcfd3b5e8e2e72053af2248114222e6e
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 01:27:34 -0400

    creating skel

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 
 CHARTVERSION=`cat VERSION | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/\1/"`
 LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/"`
+echo $LOCALREVISION
 
 LOCALREVISION=`echo $LOCALREVISION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-
+echo $LOCALREVISION
 
 rm jenkins-${CHARTVERSION}.tgz
 rm -rf charts/jenkins
@@ -17,7 +18,7 @@ tar -zxvf jenkins-${CHARTVERSION}.tgz -C charts/
 
 cp values.yaml charts/jenkins/
 
-cat charts/jenkins/Chart.yaml | sed -re "s/^LOCALREVISION: ([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/LOCALREVISION: \1-${LOCALREVISION}/" > charts/jenkins/Chart.yaml.tmp && mv charts/jenkins/Chart.yaml.tmp charts/jenkins/Chart.yaml
+cat charts/jenkins/Chart.yaml | sed -re "s/^version: ([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/version: \1-${LOCALREVISION}/" > charts/jenkins/Chart.yaml.tmp && mv charts/jenkins/Chart.yaml.tmp charts/jenkins/Chart.yaml
 
 helm package charts/*
 
