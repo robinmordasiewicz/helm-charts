42308efef759e96f5775f877626c47bad8d14518
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 19:47:10 -0400

    creating skel

--- increment-version.sh
+++ increment-version.sh
@@ -5,3 +5,7 @@ CHARTVERSION=`cat VERSION.helmchart | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9
 LOCALREVISION=`cat VERSION.helmchart | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
 echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION.helmchart
 
+
+CONTAINERVERSION=`cat VERSION.container`
+
+cat values.yaml | sed -re "s/(tag:) \"[0-9]*\.[0-9]*\.[0-9]*-[0-9]*\"/\1 \"${CONTAINERVERSION}\"/" | sed -re "s/(tagLabel:) [0-9]*\.[0-9]*\.[0-9]*-[0-9]*/\1 ${CONTAINERVERSION}/" > values.yaml.tmp && mv values.yaml.tmp values.yaml
