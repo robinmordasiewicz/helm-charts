f631f265cd56e9f9a5fbbdf3eff44a16cc58b7f8
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 11:51:37 -0400

    creating skel

--- increment-version.sh
+++ increment-version.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #
 
-CHARTVERSION=`cat VERSION | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/\1/"`
-LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/"`
-
-LOCALREVISION=`echo $LOCALREVISION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION
+CHARTVERSION=`cat VERSION.helmchart | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/\1/"`
+LOCALREVISION=`cat VERSION.helmchart | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION.helmchart
 
