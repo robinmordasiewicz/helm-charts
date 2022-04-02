7644613786273c526158893d98995756a2a653e4
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 19:56:09 -0400

    creating skel

--- increment-version.sh
+++ increment-version.sh
@@ -5,6 +5,7 @@ CHARTVERSION=`cat VERSION.helmchart | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9
 LOCALREVISION=`cat VERSION.helmchart | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
 echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION.helmchart
 
+curl https://raw.githubusercontent.com/robinmordasiewicz/jenkins-container/main/VERSION > VERSION.container
 
 CONTAINERVERSION=`cat VERSION.container`
 
