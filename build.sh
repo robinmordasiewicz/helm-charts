ad293d5afbea0fc80146fdab45e7aed49a6dd283
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 02:09:23 -0400

    creating skel

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@
 CHARTVERSION=`cat VERSION | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/\1/"`
 LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/"`
 
-LOCALREVISION=`echo $LOCALREVISION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION
-
 rm jenkins-${CHARTVERSION}.tgz
 rm -rf charts/jenkins
 
--- increment-version.sh
+++ increment-version.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
 
-cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > VERSION.tmp && mv VERSION.tmp VERSION
+CHARTVERSION=`cat VERSION | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/\1/"`
+LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/"`
 
-version=`cat VERSION`
+LOCALREVISION=`echo $LOCALREVISION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION
 
-cat charts/jenkins/Chart.yaml | sed -re "s/^version: ([0-9]+\.[0-9]+\.[0-9]+)/version: \1-${version}/" > charts/jenkins/Chart.yaml.tmp && mv charts/jenkins/Chart.yaml.tmp charts/jenkins/Chart.yaml
-
-#cat VERSION
