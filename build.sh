dbbf4f3c2b75c48d54e783167939e7ed8fbfca52
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 01:30:08 -0400

    creating skel

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 
 CHARTVERSION=`cat VERSION | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/\1/"`
 LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/"`
+echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION
 echo $LOCALREVISION
 
 LOCALREVISION=`echo $LOCALREVISION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
