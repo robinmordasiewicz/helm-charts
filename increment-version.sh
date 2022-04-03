0828dfcd2eab89e86f077cf557502bc75c4bea63
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-02 23:40:13 -0400

    creating skel

--- increment-version.sh
+++ increment-version.sh
@@ -3,13 +3,12 @@
 
 CHARTVERSION=`cat VERSION.helmchart | sed -re "s/^([0-9]+\.[0-9]+\.[0-9]+)-*[0-9]*/\1/"`
 LOCALREVISION=`cat VERSION.helmchart | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+
 echo "${CHARTVERSION}-${LOCALREVISION}" > VERSION.helmchart
 
 curl -s https://raw.githubusercontent.com/robinmordasiewicz/jenkins-container/main/VERSION > VERSION.container
 
 CONTAINERVERSION=`cat VERSION.container`
 
-#cat values.yaml | sed -re "s/(tag:) \"[0-9]*\.[0-9]*\.[0-9]*-[0-9]*\"/\1 \"${CONTAINERVERSION}\"/" | sed -re "s/(tagLabel:) [0-9]*\.[0-9]*\.[0-9]*-[0-9]*/\1 ${CONTAINERVERSION}/" > values.yaml.tmp && mv values.yaml.tmp values.yaml
-
-cat values.yaml | sed -e "s/tag:.*/  tag: \"${CONTAINERVERSION}\"/" | sed -e "s/tagLabel:.*/  tagLabel: ${CONTAINERVERSION}/" > values.yaml.tmp && mv values.yaml.tmp values.yaml
+cat values.yaml | sed -e "s/tag:.*/tag: \"${CONTAINERVERSION}\"/" | sed -e "s/tagLabel:.*/tagLabel: ${CONTAINERVERSION}/" > values.yaml.tmp && mv values.yaml.tmp values.yaml
 
